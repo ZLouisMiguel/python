6dd3d940e9e12d8d3dc37ab990b923b027470ff5
ZLouisMiguel <zigirumugabelouismiguel@gmail.com>
2026-01-25 18:46:32 +0200

    Implementations of python basics

---
+++ basics.py
@@ -0,0 +1,31 @@
+# #execution order
+# print("Line 1")
+# print("Line 2")
+# print("Line 3")
+
+
+# #datatypes
+# print("words") #string
+# print(10) #integer
+# print(10.1) #float
+# print(-10) #negative integer
+
+# #operations
+# print(3+3)
+# print(3-3)
+# print(3*3)
+# print(3/3)
+# print(10 + 0.2324)
+# print(10 * "hello")
+
+
+
+# #order of operations -> * / : high priority , +- lower priority
+# print(13 - 3 * 4 + 2 )
+
+
+#variables
+
+test = 123
+test += 50
+print(test + 10)
