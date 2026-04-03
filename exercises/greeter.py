d159f64f9a323169475fc5bb672e3641558771a3
ZLouisMiguel <zigirumugabelouismiguel@gmail.com>
2026-04-03 11:13:34 +0200

    Exercises folder to practice and solve questions

--- exercises/greeter.py
+++ exercises/greeter.py
@@ -1,7 +1,11 @@
 #Exercise to practice basics
+import random
 
-user_name = input("Enter your name: ")
+user_name = input("What is your name ? : ")
 
+greetings = ["Hello there, {}" , "Yoo!! Wassup {}" , "Konnichiwa, {}-desu!"]
 
-greetings = ["Hello there" , "Yoo!! Wassup" , "Konnichiwa"]
+random_index = random.randint(0,len(greetings)-1)
+    
+print(greetings[random_index].format(user_name))
 
