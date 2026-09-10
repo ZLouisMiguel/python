ba52dcb1ede898bcdd093d964e450990869fef58
ZLouisMiguel <zigirumugabelouismiguel@gmail.com>
2026-09-10 16:29:44 +0200

    feat(sol): my solution to ps1a

---
+++ ps1/ps1a.py
@@ -0,0 +1,19 @@
+portion_down_payment = 0.25
+current_savings = 0.0
+r = 0.04
+
+annual_salary = float(input("Enter your annual salary: "))
+portion_saved = float(input("Enter the percent of your salary to save, as a decimal: "))
+total_cost = float((input("Enter the cost of your dream home: ")))
+
+monthly_salary = annual_salary / 12
+down_payment = total_cost * portion_down_payment
+months = 0
+
+while current_savings < down_payment:
+    interest = current_savings * r / 12
+    saved_amount = monthly_salary * portion_saved
+    current_savings += interest + saved_amount
+    months += 1
+
+print("Number of months: ", months)
