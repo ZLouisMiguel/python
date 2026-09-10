0fc8650a3f2f3cc740c6ae07400b11d53e9b163e
ZLouisMiguel <zigirumugabelouismiguel@gmail.com>
2026-09-10 15:41:02 +0200

    feat: started code for python , my solution to problem set 0

---
+++ ps0/sol.py
@@ -0,0 +1,10 @@
+import math
+
+print("Input a number x: ")
+x = int(input())
+print("Input a number y: ")
+y = int(input())
+
+print("The result of x**y is: ", x**y)
+
+print("The log (base 2) of x is: ", math.log(x,2))
